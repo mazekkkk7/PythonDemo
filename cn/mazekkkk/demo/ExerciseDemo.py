b6370a1952cbb1c0c1069ab5a6e40007d66a32fb
maze <mazekkkk@hotmail.com>
2017-02-19 22:56:13 +0800

    modify by maze 2017-02-19 测试提交

--- cn/mazekkkk/demo/ExerciseDemo.py
+++ cn/mazekkkk/demo/ExerciseDemo.py
@@ -1,4 +1,3 @@
-
 # x = input("x:")
 # y = input("y:")
 # print("the multiplication result")
@@ -13,4 +12,37 @@
 
 # print 'C:\nowhere'
 # print r'C:\nowhere'
+
+# maze = ['maze',26]
+# chy = ['cy',27]
+# database = [maze,chy]
+# print database
+
+# greeting = 'Hello'
+# print greeting[0]
+# print greeting[-1]
+
+# fourth = raw_input('Year:')[3]
+# print fourth
+
+# number = [1,2,3,4,5,6,7,8,9,10]
+# print number[:]
+# print number[0:10]
+# print number[0:-1]
+# print number[0:0]
+# print number[::2]
+# print number[-10::2]
+
+# numberZero = [1,2,3]
+# numberOne = [4,5,6]
+# print numberZero+numberOne
+# print 'Hello,'+'world!'
+# print numberZero * 2
+# print 'Hello ' * 5
+
+# sequence = [None] * 10
+# print sequence
+
+users = ['liyuchun','zengyike']
+print raw_input('Enter you user name:') in users
 
